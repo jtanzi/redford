be31e01461fbca89395026e44ecb02111ea1651c
Joel Tanzi <jtanziredux@gmail.com>
2015-05-05 19:00:29 -0500

    Fixed caption display and fading

--- app.js
+++ app.js
@@ -4,11 +4,21 @@
 var $image = $("<img>");
 var $caption = $("<p></p>");
 var mainimage = $("#main-image img");
+var $artistInfo = $("<p></p>");
+
+//Fade h1 on load
+$("html").ready(function() {
+	$("h1").slideDown(600);
+	mainimage.hide();
+	mainimage.delay(1000).fadeIn(600);
+});
                  
 //Capture the click event on a link to an image.
 $("#sidebar a").click(function(event) {
   event.preventDefault();
 	mainimage.hide();
 	mainimage.attr("src", "img/" + $(this).text() + ".jpg");
+	$("#main-image").append($caption);	
+	$caption.text($(this).text());
 	mainimage.fadeIn("slow");
 });
